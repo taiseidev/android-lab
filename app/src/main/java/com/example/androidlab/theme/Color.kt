20c597fa6860d2cc91f5b6b7e63dd5c5e3c1203f
onishi taisei <taisei.developer@gmail.com>
2025-01-18 23:10:53 +0900

    🚜 refactor: Move them files.

--- app/src/main/java/com/example/androidlab/theme/Color.kt
+++ app/src/main/java/com/example/androidlab/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.androidlab.ui.theme
+package com.example.androidlab.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/androidlab/theme/Theme.kt
+++ app/src/main/java/com/example/androidlab/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.androidlab.ui.theme
+package com.example.androidlab.theme
 
 import android.app.Activity
 import android.os.Build
